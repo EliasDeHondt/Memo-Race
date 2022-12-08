b02303c0b76f44b5c838fa41259dab4214cd32eb
v <zeppi_IP@protonmail.com>
2022-12-08 16:31:25 +0100

    todo

--- Demo.java
+++ Demo.java
@@ -3,6 +3,21 @@
  * 8/12/2022
  */
 public class Demo {
+    /*TODO
+    -Voorzie een startscherm waar je de namen van de spelers kan ingeven.
+    -Elke speler kan een avatar kiezen of zelf een foto toevoegen.
+    -Voorzie een lijst op het scherm met eerder aangemaakte spelers. Sla deze op in een bestand.
+    -Zorg voor een mooi grafische vormgeving.
+    -Tijdens het spelverloop moet volgende zichtbaar zijn:
+    o Een dobbelsteen
+    o Het speelveld met in het midden de omgedraaide kaarten
+    o Per speler zijn naam, de avatar en de gewonnen kaarten
+    o Een pion die automatisch verplaatst na een worp
+    - Kaarten moeten met de muis omgedraaid kunnen worden.
+    - Als een figuur gevonden is moet het overeenkomstige vak een alternatieve opmaak (bv.
+    donkerder) krijgen zodat duidelijk is dat de pion deze vakken mag overslaan.
+    - Maak visueel zichtbaar welke speler aan de beurt is.
+    */
     public static void main(String[] args) {
 
     }
