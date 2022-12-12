cb830c05953096c15aed0040a4f25bae333ddce2
EliasDeHondt <Elias.dehondt@outlook.com>
2022-12-12 11:29:08 +0100

    Commit van Elias De Hondt

--- Demo/Demo.java
+++ Demo/Demo.java
@@ -9,6 +9,6 @@ import Spel.*;
 public class Demo {
     public static void main(String[] args) {
         Speelbord speelbord = new Speelbord();
-        speelbord.start();
+        speelbord.printBord();
     }
 }
