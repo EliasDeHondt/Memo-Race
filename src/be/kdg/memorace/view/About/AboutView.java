3ceeb54f939c21c6027c497227d4a9d2cbca0bfd
EliasDeHondt <Elias.dehondt@outlook.com>
2023-03-01 14:48:29 +0100

    Commit ...

---
+++ src/be/kdg/memorace/view/About/AboutView.java
@@ -0,0 +1,13 @@
+package be.kdg.memorace.view.About;
+
+/**
+ * Van Elias De Hondt
+ * 1/03/2023
+ */
+public class AboutView {
+    // Attributes
+
+    // Constructors
+
+    // Methods
+}
---
+++ src/be/kdg/memorace/view/GameLog/GameLogPresenter.java
@@ -0,0 +1,13 @@
+package be.kdg.memorace.view.GameLog;
+
+/**
+ * Van Elias De Hondt
+ * 1/03/2023
+ */
+public class GameLogPresenter {
+    // Attributes
+
+    // Constructors
+
+    // Methods
+}
